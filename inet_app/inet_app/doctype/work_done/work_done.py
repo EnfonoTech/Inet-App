772a12bae6fb2a6f02ff5bae602f285f81bc5b13
sayanthns <sayanth2k16@gmail.com>
2026-04-04 22:20:33 +0300

    feat: add Work Done doctype with revenue/cost/margin calculation

---
+++ inet_app/inet_app/doctype/work_done/work_done.py
@@ -0,0 +1,9 @@
+import frappe
+from frappe.model.document import Document
+from frappe.utils import flt
+
+class WorkDone(Document):
+    def before_save(self):
+        self.revenue_sar = flt(self.billing_rate_sar) * flt(self.executed_qty)
+        self.total_cost_sar = flt(self.team_cost_sar) + flt(self.subcontract_cost_sar)
+        self.margin_sar = flt(self.revenue_sar) - flt(self.total_cost_sar)
